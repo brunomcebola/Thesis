693aeaf9a011b325ed6eb51b47edf6ec1f5cbd2b
Bruno Cebola <bruno.m.cebola@gmail.com>
2023-10-27 23:41:17 +0100

    Passed DEFAULTS from StreamFormat to Camera

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 """
 Module for testing
 """
-# import pyrealsense2.pyrealsense2 as rs
+import pyrealsense2.pyrealsense2 as rs
 from camera import Camera, StreamType, StreamFormat
 
-print(isinstance(StreamType.DEPTH.value, list))
+c = Camera(
+    "123",
+    "123",
+    StreamType.DEPTH,
+    {StreamType.DEPTH: StreamFormat(rs.format.z16, (640, 480), 30)},
+)
+
+print(Camera.DEFAULTS)
