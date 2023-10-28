69e6739d2fdcdc581a61eb055bb558992c0b3f29
Bruno Cebola <bruno.m.cebola@gmail.com>
2023-10-28 02:43:28 +0100

    Camera module refactor finished (with docstrings updated. Missing implementation of get_frames due to dependeny on stream type handler

--- test.py
+++ test.py
@@ -5,13 +5,22 @@ import pyrealsense2.pyrealsense2 as rs
 from camera import Camera, StreamType, StreamConfig
 
 c1 = Camera(
-    "123",
     "135522077203",
     StreamType.DEPTH,
-    {
-        StreamType.DEPTH: StreamConfig(rs.format.z16, (640, 480), 30)
-    },
+    {StreamType.DEPTH: StreamConfig(rs.format.z16, (640, 480), 30)},
+    "c1",
 )
+
+
+    c2 = Camera(
+        "135522077203",
+        StreamType.DEPTH,
+        {StreamType.DEPTH: StreamConfig(rs.format.z16, (640, 480), 30)},
+        "c2",
+    )
+
+
+print("created")
 
 try:
     c1.change_stream_configs(
