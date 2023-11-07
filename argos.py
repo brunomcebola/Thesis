90d8319849b8fec86834075d468c2e75fc4a7b91
Bruno Cebola <bruno.m.cebola@gmail.com>
2023-11-07 16:04:47 +0000

    aquire mode main thread able to log and stop based on flag

--- argos.py
+++ argos.py
@@ -30,25 +30,11 @@ if __name__ == "__main__":
     cmd_args = parser.get_args()
 
     if cmd_args.mode in ["aquire", "a"]:
-        handler = aquire.Aquire(**cmd_args.__dict__)
+        mode = aquire.Aquire(**cmd_args.__dict__)
 
-        prompt = utils.get_user_confirmation(  # pylint: disable=invalid-name
-            "Do you wish to continue?"
-        )
-        print()
+        mode.run()
 
-        if prompt:
-            pass
-            # t1 = threading.Thread(target=aquire.aquire)
-            # t1.start()
-
-            # if not get_user_confirmation("Do you wish to continue?"):
-            #     aquire.STOP_FLAG = True
-            #     t1.join()
-            #     exit(0)
-        else:
-            utils.print_info("Exiting aquire mode...")
-            exit(0)
+        # mode.stop()
 
     elif cmd_args.mode in ["train", "t"]:
         utils.print_info("Entering train mode...")
@@ -67,60 +53,14 @@ if __name__ == "__main__":
             exit(1)
 
         if args["mode"] == "aquire":
-            handler = aquire.Aquire(**args)
+            mode = aquire.Aquire(**args)
 
-            prompt = utils.get_user_confirmation(  # pylint: disable=invalid-name
-                "Do you wish to continue?"
-            )
-            print()
+            mode.run()
 
-            if prompt:
-                pass
-                # t1 = threading.Thread(target=aquire.aquire)
-                # t1.start()
+            import time
 
-                # if not get_user_confirmation("Do you wish to continue?"):
-                #     aquire.STOP_FLAG = True
-                #     t1.join()
-                #     exit(0)
-            else:
-                utils.print_info("Exiting aquire mode...")
-                exit(0)
+            time.sleep(10)
 
-        # import intel
-        # import pyrealsense2.pyrealsense2 as rs
-
-        # try:
-        #     aquireNamespace = aquire.AquireNamespace(
-        #         args["output_folder"],
-        #         args["op_times"],
-        #         [cam["sn"] for cam in args["cameras"]],
-        #         [cam["name"] for cam in args["cameras"]],
-        #         None,
-        #         [
-        #             {
-        #                 intel.StreamType[config.upper()]: intel.StreamConfig(
-        #                     rs.format.z16
-        #                     if cam["stream_config"][config]["format"] == "z16"
-        #                     else rs.format.rgb8,
-        #                     (
-        #                         cam["stream_config"][config]["width"],
-        #                         cam["stream_config"][config]["height"],
-        #                     ),
-        #                     cam["stream_config"][config]["fps"],
-        #                 )
-        #                 for config in cam["stream_config"]
-        #             }
-        #             for cam in args["cameras"]
-        #         ],
-        #     )
-        # except Exception as e:
-        #     utils.print_error(str(e))
-        #     exit(1)
-
-        # print()
-        # utils.print_info("Aquire mode settings:")
-        # print(aquireNamespace)
-        # print()
+            mode.stop()
     else:
         exit(0)
