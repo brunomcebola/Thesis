bee428e26e3045844c3e174bba801672aee7eea9
Bruno Cebola <bruno.m.cebola@gmail.com>
2023-12-19 16:56:47 +0000

    updated master machine scripts

--- bash/.add_pi.sh
+++ bash/.add_pi.sh
@@ -17,10 +17,16 @@ read -p "What is the raspberry pi MAC address? " mac_address
 
 echo ""
 
-if [ -d "/mnt/disk/${hostname}" ]
+if [ -d "/rpi/nfs/${hostname}" ]
 then
 	echo -e "${B_RED}Error:${NC} A raspberry pi with the specified hostname (${hostname}) already exists."
 	exit 1
+fi
+
+if [ -d "/rpi/tftpboot/${serial_number}" ]
+then
+        echo -e "${B_RED}Error:${NC} A raspberry pi with the specified serial number (${serial_number}) already exists."
+        exit 1
 fi
 
 echo -e "${U_WHITE}Entering sudo mode${NC}"
@@ -29,49 +35,56 @@ sudo su << EOF
 	echo
 
 	echo -e "${U_WHITE}Creating nfs directory${NC}"
-	echo '$ mkdir -p /mnt/disk/${hostname}'
-	mkdir -p /mnt/disk/${hostname}
+	echo '$ mkdir -p /rpi/nfs/${hostname}'
+	mkdir -p /rpi/nfs/${hostname}
 	echo
 
-	echo -e "${U_WHITE}Copying master nfs${NC}"
-	echo '$ cp -a /mnt/disk/master/* /mnt/disk/${hostname}'
-	cp -a /mnt/disk/master/* /mnt/disk/${hostname}
+	echo -e "${U_WHITE}Copying base nfs${NC}"
+	echo '$ cp -a /rpi/nfs/base/* /rpi/nfs/${hostname}'
+	cp -a /rpi/nfs/base/* /rpi/nfs/${hostname}
 	echo
 
 	echo -e "${U_WHITE}Creating tftp boot directory${NC}"
-	echo '$ mkdir -p /mnt/disk/tftpboot/${serial_number}'
-	mkdir -p /mnt/disk/tftpboot/${serial_number}
+	echo '$ mkdir -p /rpi/tftpboot/${serial_number}'
+	mkdir -p /rpi/tftpboot/${serial_number}
 	echo
 
 	echo -e "${U_WHITE}Adding new raspberry to fstab${NC}"
-	echo '$ echo "/mnt/disk/${hostname}/boot /mnt/disk/tftpboot/${serial_number} none defaults,bind 0 0" >> /etc/fstab'
-	echo "/mnt/disk/${hostname}/boot /mnt/disk/tftpboot/${serial_number} none defaults,bind 0 0" >> /etc/fstab
+	echo '$ echo "/rpi/nfs/${hostname}/boot /rpi/tftpboot/${serial_number} none defaults,bind 0 0" >> /etc/fstab'
+	echo "/rpi/nfs/${hostname}/boot /rpi/tftpboot/${serial_number} none defaults,bind 0 0" >> /etc/fstab
 	echo
 
 	echo -e "${U_WHITE}Adding new raspberry to exports${NC}"
-	echo '$ echo "/mnt/disk/${hostname} 192.168.50.0/24(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports'
-	echo "/mnt/disk/${hostname} 192.168.50.0/24(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
+	echo '$ echo "/rpi/nfs/${hostname} 192.168.50.0/24(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports'
+	echo "/rpi/nfs/${hostname} 192.168.50.0/24(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
 	echo
 
 	echo -e "${U_WHITE}Changing boot cmdline.txt file${NC}"
-	echo '$ echo "console=serial0,115200 console=tty root=/dev/nfs nfsroot=192.168.50.1:/mnt/disk/${hostname},vers=3,proto=tcp rw ip=dhcp rootwait" > /mnt/disk/${hostname}/boot/cmdline.txt'
-	echo "console=serial0,115200 console=tty root=/dev/nfs nfsroot=192.168.50.1:/mnt/disk/${hostname},vers=3,proto=tcp rw ip=dhcp rootwait" > /mnt/disk/${hostname}/boot/cmdline.txt
+	echo '$ echo "console=serial0,115200 console=tty root=/dev/nfs nfsroot=192.168.50.1:/rpi/nfs/${hostname},vers=3,proto=tcp rw ip=dhcp rootwait" > /rpi/nfs/${hostname}/boot/cmdline.txt'
+	echo "console=serial0,115200 console=tty root=/dev/nfs nfsroot=192.168.50.1:/rpi/nfs/${hostname},vers=3,proto=tcp rw ip=dhcp rootwait" > /rpi/nfs/${hostname}/boot/cmdline.txt
 	echo
 
 	echo -e "${U_WHITE}Changing hostname${NC}"
-	echo '$ echo "${hostname}" > /mnt/disk/${hostname}/etc/hostname'
-	echo "${hostname}" > /mnt/disk/${hostname}/etc/hostname
+	echo '$ echo "${hostname}" > /rpi/nfs/${hostname}/etc/hostname'
+	echo "${hostname}" > /rpi/nfs/${hostname}/etc/hostname
+	echo "$ sed -i 's/^127.0.1.1.*/127.0.1.1       test/' /rpi/nfs/${hostname}/etc/hosts"
+	sed -i 's/^127.0.1.1.*/127.0.1.1       ${hostname}/' /rpi/nfs/${hostname}/etc/hosts
 	echo
 
 	echo -e "${U_WHITE}Adding raspberry to dhcpd.conf${NC}"
-	echo "$ sed 's/# nodes/host ${hostname} {\n          option root-path \"\/mnt\/disk\/tftpboot\/\";\\n          hardware ethernet ${mac_address};\\n          option option-43 \"Raspberry Pi Boot\";\\n          option option-66 \"192.168.50.1\";\\n          next-server 192.168.50.1;\\n          fixed-address ${fixed_ip};\\n          option host-name \"${hostname}\";\\n      }\\n\\n      # nodes/' /etc/dhcp/dhcpd.conf"
-	sed -i 's/# nodes/host ${hostname} {\n          option root-path "\/mnt\/disk\/tftpboot\/";\n          hardware ethernet ${mac_address};\n          option option-43 "Raspberry Pi Boot";\n          option option-66 "192.168.50.1";\n          next-server 192.168.50.1;\n          fixed-address ${fixed_ip};\n          option host-name "${hostname}";\n      }\n\n      \# nodes/' /etc/dhcp/dhcpd.conf
+	echo "$ sed -i 's/# nodes/host ${hostname} {\n          option root-path \"\/rpi\/tftpboot\/\";\\n          hardware ethernet ${mac_address};\\n          option option-43 \"Raspberry Pi Boot\";\\n          option option-66 \"192.168.50.1\";\\n          next-server 192.168.50.1;\\n          fixed-address ${fixed_ip};\\n          option host-name \"${hostname}\";\\n      }\\n\\n      # nodes/' /etc/dhcp/dhcpd.conf"
+	sed -i 's/# nodes/host ${hostname} {\n          option root-path "\/rpi\/tftpboot\/";\n          hardware ethernet ${mac_address};\n          option option-43 "Raspberry Pi Boot";\n          option option-66 "192.168.50.1";\n          next-server 192.168.50.1;\n          fixed-address ${fixed_ip};\n          option host-name "${hostname}";\n      }\n\n      \# nodes/' /etc/dhcp/dhcpd.conf
 	echo
 
 	echo -e "${U_WHITE}Adding raspberry to /etc/hosts${NC}"
 	echo 'echo "${fixed_ip}   ${hostname}" >> /etc/hosts'
 	echo "${fixed_ip}   ${hostname}" >> /etc/hosts
 	echo
+
+	echo -e "${U_WHITE}Adding raspberry to /home/thales/.rpi_pssh_hosts${NC}"
+        echo 'echo "${hostname}" >> /home/thales/.rpi_pssh_hosts'
+        echo "${hostname}" >> /home/thales/.rpi_pssh_hosts
+        echo
 
 	echo -e "${U_WHITE}Exiting sudo mode${NC}"
 	echo '$ exit'
@@ -80,6 +93,10 @@ EOF
 echo
 
 echo -e "${B_GREEN}Success:${NC} New raspberry added to the cluster (${hostname})"
+echo
+
+echo -e "${B_CYAN}Plug the raspberry to a ethernet cable connected to the switch and"
+echo -e "run 'reboot_cluster' in order for the changes to take effect!${NC}"
 echo
 
 echo -e "${B_CYAN}Exiting Pi Cluster Wizard!${NC}"
--- bash/.forward_ipv4.sh
+++ bash/.forward_ipv4.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo iptables -A FORWARD -o eno1 -i enp8s5 -s 192.168.50.0/24 -m conntrack --ctstate NEW -j ACCEPT
-sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-sudo iptables -t nat -F POSTROUTING
-sudo iptables -t nat -A POSTROUTING -o eno1 -j MASQUERADE
+sudo -S  iptables -A FORWARD -o eno1 -i enp8s5 -s 192.168.50.0/24 -m conntrack --ctstate NEW -j ACCEPT
+sudo -S iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo -S iptables -t nat -F POSTROUTING
+sudo -S iptables -t nat -A POSTROUTING -o eno1 -j MASQUERADE
 
-sudo iptables-save | sudo tee /etc/iptables.sav
+sudo -S iptables-save | sudo -S tee /etc/iptables.sav
 
-sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+sudo -S sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
