d26b16b8948801959e1c9fa7cf351d8473d90b29
Bruno Cebola <bruno.m.cebola@gmail.com>
2024-04-29 00:19:02 +0100

    Added flag in ArgosPrinter to add space before and/or after

--- argos/argos/printer.py
+++ argos/argos/printer.py
@@ -2,6 +2,9 @@
 This module contains the ArgosPrinter class.
 """
 
+from __future__ import annotations
+
+from enum import Enum
 from colorama import Fore, Style
 
 
@@ -10,20 +13,43 @@ class ArgosPrinter:
     A class that contains static methods for printing messages to the console.
     """
 
+    class Space(Enum):
+        """
+        An enumeration that defines the space options for the print methods.
+        """
+
+        NONE = 0
+        BEFORE = 1
+        AFTER = 2
+        BOTH = 3
+
     @classmethod
-    def print_header(cls) -> None:
+    def _base_print(cls, message: str, space: Space = Space.NONE) -> None:
+        """
+        Prints a base message.
+        """
+
+        if space == cls.Space.BEFORE or space == cls.Space.BOTH:
+            print()
+
+        print(message)
+
+        if space == cls.Space.AFTER or space == cls.Space.BOTH:
+            print()
+
+    @classmethod
+    def print_header(cls, space: Space = Space.NONE) -> None:
         """
         Prints the program header.
         """
-        print(
-            Style.BRIGHT
-            + Fore.LIGHTBLUE_EX
-            + "Welcome to Argos, Real-time Image Analysis for Fraud Detection!"
-            + Style.RESET_ALL
+
+        cls._base_print(
+            f"{Style.BRIGHT + Fore.LIGHTBLUE_EX}Welcome to Argos, Real-time Image Analysis for Fraud Detection!{Style.RESET_ALL}",  # pylint: disable=line-too-long
+            space,
         )
 
     @classmethod
-    def print_info(cls, message: str) -> None:
+    def print_info(cls, message: str, space: Space = Space.NONE) -> None:
         """
         Prints an info message.
 
@@ -31,10 +57,13 @@ class ArgosPrinter:
         -----
             - message: The info message to be printed.
         """
-        print(f"{Fore.LIGHTCYAN_EX + Style.BRIGHT}Info:{Style.RESET_ALL} {message}")
+        cls._base_print(
+            f"{Fore.LIGHTCYAN_EX + Style.BRIGHT}Info:{Style.RESET_ALL} {message}",
+            space,
+        )
 
     @classmethod
-    def print_success(cls, message: str) -> None:
+    def print_success(cls, message: str, space: Space = Space.NONE) -> None:
         """
         Prints a success message.
 
@@ -42,10 +71,13 @@ class ArgosPrinter:
         -----
             - message: The success message to be printed.
         """
-        print(f"{Fore.LIGHTGREEN_EX + Style.BRIGHT}Success:{Style.RESET_ALL} {message}")
+        cls._base_print(
+            f"{Fore.LIGHTGREEN_EX + Style.BRIGHT}Success:{Style.RESET_ALL} {message}",
+            space,
+        )
 
     @classmethod
-    def print_warning(cls, message: str) -> None:
+    def print_warning(cls, message: str, space: Space = Space.NONE) -> None:
         """
         Prints a warning message.
 
@@ -53,10 +85,13 @@ class ArgosPrinter:
         -----
             - message: The warning message to be printed.
         """
-        print(f"{Fore.LIGHTYELLOW_EX + Style.BRIGHT}Warning:{Style.RESET_ALL} {message}")
+        cls._base_print(
+            f"{Fore.LIGHTYELLOW_EX + Style.BRIGHT}Warning:{Style.RESET_ALL} {message}",
+            space,
+        )
 
     @classmethod
-    def print_error(cls, message: str) -> None:
+    def print_error(cls, message: str, space: Space = Space.NONE) -> None:
         """
         Prints an error message.
 
@@ -64,4 +99,7 @@ class ArgosPrinter:
         -----
             - message: The error message to be printed.
         """
-        print(f"{Fore.RED + Style.BRIGHT}Error:{Style.RESET_ALL} {message}")
+        cls._base_print(
+            f"{Fore.RED + Style.BRIGHT}Error:{Style.RESET_ALL} {message}",
+            space,
+        )
