ced9462ec6f39c649e648c51c345e81b3ad6c5fb
Bruno Cebola <bruno.m.cebola@gmail.com>
2024-04-29 01:06:58 +0100

    New parser struct working with dynamic creation on head and node subpackages.

--- argos/argos/master/__main__.py
+++ argos/argos/master/__main__.py
@@ -10,7 +10,32 @@ from ..printer import ArgosPrinter
 from ..parser import ArgosParser
 
 
-from .web import run_interface
+# from .web import run_interface
+
+
+def _generate_parser() -> ArgosParser:
+    """
+    Generates the ArgosParser object.
+
+    Returns:
+    --------
+        - ArgosParser: The generated ArgosParser object.
+    """
+
+    parser = ArgosParser("(master)")
+
+    parser.add_parser(
+        "root",
+        "command",
+        "interface",
+        True,
+        {
+            "description": "Interface launcher",
+            "help": "Mode to start the Argos Interface.",
+        },
+    )
+
+    return parser
 
 
 def main():
@@ -21,62 +46,60 @@ def main():
     load_dotenv()
 
     try:
-        parser = ArgosParser()
+        parser = _generate_parser()
 
         cmd_line_args = parser.get_args()
 
-        print()
-        ArgosPrinter.print_header()
-        print()
+        ArgosPrinter.print_header(ArgosPrinter.Space.BOTH)
 
-        if cmd_line_args.resource == "interface":
-            run_interface()
+        # if cmd_line_args.resource == "interface":
+        #     run_interface()
 
-        elif cmd_line_args.resource == "service":
-            service = cmd_line_args.service
-            command = cmd_line_args.command
+        # elif cmd_line_args.resource == "service":
+        #     service = cmd_line_args.service
+        #     command = cmd_line_args.command
 
-            cmd_line_args = cmd_line_args.__dict__
+        #     cmd_line_args = cmd_line_args.__dict__
 
-            del cmd_line_args["resource"]
-            del cmd_line_args["service"]
-            del cmd_line_args["command"]
+        #     del cmd_line_args["resource"]
+        #     del cmd_line_args["service"]
+        #     del cmd_line_args["command"]
 
-            if command == "run":
-                if "yaml" in cmd_line_args and cmd_line_args["yaml"] is not None:
-                    args = MAP_TO_SERVICE_NAMESPACE_CLASS[service].from_yaml(cmd_line_args["yaml"])
-                else:
-                    args = MAP_TO_SERVICE_NAMESPACE_CLASS[service](**cmd_line_args)
+        #     if command == "run":
+        #         if "yaml" in cmd_line_args and cmd_line_args["yaml"] is not None:
+        #             args = MAP_TO_SERVICE_NAMESPACE_CLASS[service].from_yaml(cmd_line_args["yaml"])
+        #         else:
+        #             args = MAP_TO_SERVICE_NAMESPACE_CLASS[service](**cmd_line_args)
 
-                # Run the service
+        #         # Run the service
 
-                Printer.print_info(f"{MAP_TO_SERVICE_CLASS[service].__name__} settings")
-                print(args)
-                print()
+        #         ArgosPrinter.print_info(f"{MAP_TO_SERVICE_CLASS[service].__name__} settings")
+        #         print(args)
+        #         print()
 
-                while True:
-                    response = input("Do you wish to start the data acquisition? (y/n): ")
+        #         while True:
+        #             response = input("Do you wish to start the data acquisition? (y/n): ")
 
-                    if response in ["y", "Y", "yes", "Yes", "YES"]:
-                        MAP_TO_SERVICE_CLASS[service](args).run()
+        #             if response in ["y", "Y", "yes", "Yes", "YES"]:
+        #                 MAP_TO_SERVICE_CLASS[service](args).run()
 
-                    elif response in ["n", "N", "no", "No", "NO"]:
-                        return False
+        #             elif response in ["n", "N", "no", "No", "NO"]:
+        #                 return False
 
-                    else:
-                        Printer.print_warning("Invalid response. Please enter y or n.")
-                        print()
+        #             else:
+        #                 Printer.print_warning("Invalid response. Please enter y or n.")
+        #                 print()
 
-            elif command == "logs":
-                MAP_TO_SERVICE_CLASS[service].logs(cmd_line_args["logs_dest"])
+        #     elif command == "logs":
+        #         MAP_TO_SERVICE_CLASS[service].logs(cmd_line_args["logs_dest"])
 
-            else:
-                Printer.print_error("Invalid command!\n")
+        #     else:
+        #         Printer.print_error("Invalid command!\n")
 
     except Exception as e:  # pylint: disable=broad-except
-        Printer.print_error(str(e) + "\n")
+        ArgosPrinter.print_error(str(e) + "\n")
 
-        utils.print_warning("Terminating program!\n")
+        ArgosPrinter.print_warning("Terminating program!\n")
 
         exit(1)
 
@@ -84,14 +107,11 @@ def main():
         print()
         print()
 
-        Printer.print_warning("Terminating program!\n")
+        ArgosPrinter.print_warning("Terminating program!\n")
 
         exit(1)
 
-    print()
-    print()
-
-    Printer.print_warning("Terminating program!\n")
+    ArgosPrinter.print_warning("Terminating program!\n")
 
     exit(0)
 
