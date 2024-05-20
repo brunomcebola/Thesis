fb203d5a530ae57206b71d52bc10d6c7bc4c3c12
Bruno Cebola <bruno.m.cebola@gmail.com>
2024-05-20 01:14:43 +0100

    REFACTOR (argos_node): changed __main__ file to accomodate a simpler app launch logic, removed dedicated network and terminal files (wont be needed), and integrated NodeNamespace generation to the initialization phase

--- argos/argos_node_env/argos_node/argos_node/__main__.py
+++ argos/argos_node_env/argos_node/argos_node/__main__.py
@@ -5,43 +5,12 @@ This module is the entry point of Argos, a real-time image analysis tool for fra
 from __future__ import annotations
 
 import os
+from flask import Flask
 from dotenv import find_dotenv, load_dotenv
 
 import argos_common as ac
 
-from .network import run_network
-
-
-def _generate_parser() -> ac.Parser:
-    """
-    Generates the ArgosParser object.
-
-    Returns:
-    --------
-        - ArgosParser: The generated ArgosParser object.
-    """
-
-    parser = ac.Parser()
-
-    parser.add_parser(
-        "root",
-        "network",
-        {
-            "description": "Network interface",
-            "help": "Launches the network interface.",
-        },
-    )
-
-    parser.add_parser(
-        "root",
-        "terminal",
-        {
-            "description": "Terminal interface",
-            "help": "Launches the terminal interface.",
-        },
-    )
-
-    return parser
+from .namespace import NodeNamespace
 
 
 def main():
@@ -49,48 +18,46 @@ def main():
     Main function of the program
     """
 
-    # Checks if ARGOS_ENV environment variable is set
-    # If not, defaults to 'production'
-    if "ARGOS_ENV" not in os.environ:
-        os.environ["ARGOS_ENV"] = "production"
-
     ac.Printer.print_header(ac.Printer.Space.BOTH)
 
     # Find the .env file
-    ac.Printer.print_info(f"Setting {os.environ['ARGOS_ENV'].upper()} environment!")
+    ac.Printer.print_info("Setting environment variables!")
     try:
-        env_path = find_dotenv(
-            filename=f".env.{os.environ['ARGOS_ENV']}", raise_error_if_not_found=True
-        )
+        env_path = find_dotenv(filename=".env.argos", raise_error_if_not_found=True)
     except IOError:
         ac.Printer.print_error(
-            f"Unable to set {os.environ['ARGOS_ENV'].upper()} environment!", ac.Printer.Space.AFTER
+            "Unable to setup environment varibales. No '.env.argos' file found!",
+            ac.Printer.Space.AFTER,
         )
         exit(1)
 
     # Load the environment variables from the corresponding file
     load_dotenv(dotenv_path=env_path)
-    ac.Printer.print_success(
-        f"{os.environ['ARGOS_ENV'].upper()} environment set!", ac.Printer.Space.AFTER
-    )
+    ac.Printer.print_success("Environment variables set!", ac.Printer.Space.AFTER)
 
-    # Generate the parser
-    parser = _generate_parser()
+    # Get YAML path from environment variables. If not found, print error and exit
+    yaml_path = os.getenv("ARGOS_YAML")
+    if yaml_path is None:
+        ac.Printer.print_error(
+            "No ARGOS_YAML found in environment variables! Exiting...",
+            ac.Printer.Space.AFTER,
+        )
+        exit(1)
 
-    # Parse the command line arguments
-    cmd_line_args = parser.get_args()
+    try:
+        node_namespace: NodeNamespace = ac.YAMLParser(NodeNamespace).parse(yaml_path)
+    except Exception as e:  # pylint: disable=broad-except
+        ac.Printer.print_error(str(e), ac.Printer.Space.AFTER)
+        exit(1)
 
-    if cmd_line_args.root_command == "network":
-        run_network()
-        
-        ac.Printer.print_goodbye(ac.Printer.Space.AFTER)
-        exit(0)
+    app = Flask(__name__)
 
-    if cmd_line_args.root_command == "terminal":
-        print("Terminal\n")
+    app.config["namespace"] = node_namespace
 
-        ac.Printer.print_goodbye(ac.Printer.Space.AFTER)
-        exit(0)
+    port = int(os.getenv("ARGOS_PORT", "5000"))
+
+    ac.Printer.print_info(f"Launched server on port {port}!")
+    app.run(port=port)
 
 
 if __name__ == "__main__":
