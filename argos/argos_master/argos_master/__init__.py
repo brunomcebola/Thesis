db584ee8544b635dfaf4a4152fd933f57a2cd7dc
Bruno Cebola <bruno.m.cebola@gmail.com>
2024-08-04 20:05:50 +0100

    REFACTOR (argos_master): Moved some logic to the __init__ file, in order to ensure correct access to global handlers and loggers

--- argos/argos_master/argos_master/__init__.py
+++ argos/argos_master/argos_master/__init__.py
@@ -1,3 +1,116 @@
 """
 Init file for the ARGOS master
 """
+
+import os
+import re
+import sys
+import atexit
+import signal
+import logging
+from appdirs import AppDirs
+from dotenv import find_dotenv, load_dotenv
+
+logger: logging.Logger
+
+
+def _set_environment_variables() -> None:
+    """
+    Load the environment variables from the nearest .env.argos file
+    """
+
+    # Find the .env file
+    env_path = find_dotenv(filename=".env.argos_master")
+
+    # Load the .env file
+    if env_path:
+        load_dotenv(dotenv_path=env_path)
+
+    # BASE_DIR validation
+    if not os.getenv("BASE_DIR"):
+        os.environ["BASE_DIR"] = AppDirs(__package__).user_data_dir
+
+    os.makedirs(os.environ["BASE_DIR"], exist_ok=True)
+
+    # HOST validation
+    address_regex = r"^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9]).){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])$"  # pylint: disable=line-too-long
+    if not os.getenv("HOST") or not re.match(address_regex, os.environ["HOST"]):
+        os.environ["HOST"] = "0.0.0.0"
+
+    # PORT validation
+    if (
+        not os.getenv("PORT")
+        or not os.environ["PORT"].isdigit()
+        or int(os.environ["PORT"]) > 65535
+        or int(os.environ["PORT"]) < 1024
+    ):
+        os.environ["PORT"] = "9876"
+
+    # HOT_RELOAD validation
+    if not os.getenv("HOT_RELOAD") or os.getenv("HOT_RELOAD") not in ["true", "false"]:
+        os.environ["HOT_RELOAD"] = "false"
+
+
+def _set_logger() -> None:
+    """
+    Set up the logging for the application
+    """
+
+    global logger  # pylint: disable=global-statement
+
+    # Create logger
+    logger = logging.getLogger(__package__)
+    logger.setLevel(logging.INFO)
+
+    # Create file handler
+    log_file_path = os.path.join(os.environ["BASE_DIR"], f"{__package__}.log")
+    os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
+    file_handler = logging.FileHandler(log_file_path)
+
+    # Create formatter
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+
+    # Add file handler to logger
+    logger.addHandler(file_handler)
+
+    if os.getenv("WERKZEUG_RUN_MAIN") == "true" or os.getenv("HOT_RELOAD") == "false":
+        logger.info("ARGOS master started!")
+
+
+def _set_atexit_handler() -> None:
+    """
+    Cleanup function to be called when the program is interrupted
+    """
+
+    def _callback():
+        if (
+            os.getenv("WERKZEUG_RUN_MAIN") == "true"
+            or signal.SIGINT
+            or os.getenv("HOT_RELOAD") == "false"
+        ):
+            logger.info("ARGOS master stopped!")
+
+    atexit.register(_callback)
+
+
+def _set_global_exception_hook() -> None:
+    """
+    Global exception handler
+    """
+
+    def _callback(exc_type, exc_value, exc_traceback):  # pylint: disable=unused-argument
+        logger.error("%s: %s", exc_type.__name__, exc_value)
+
+    sys.excepthook = _callback
+
+
+# Initialization code
+
+_set_environment_variables()
+
+_set_logger()
+
+_set_atexit_handler()
+
+_set_global_exception_hook()
--- argos/argos_master/argos_master/__main__.py
+++ argos/argos_master/argos_master/__main__.py
@@ -7,89 +7,14 @@ from __future__ import annotations
 import os
 import sys
 import signal
-import atexit
-import logging
 from flask import Flask
-from appdirs import AppDirs
 from flask_socketio import SocketIO
-from dotenv import find_dotenv, load_dotenv
 
 
-def _global_exception_hook(logger: logging.Logger):
+def main():
     """
-    Global exception handler
+    Main function of the program
     """
-
-    def _callback(exc_type, exc_value, exc_traceback):  # pylint: disable=unused-argument
-        logger.error(f"{exc_type.__name__}: {exc_value}")
-
-    return _callback
-
-
-def _exit_callback(logger: logging.Logger):
-    """
-    Cleanup function to be called when the program is interrupted
-    """
-
-    if os.getenv("WERKZEUG_RUN_MAIN") == "true" or signal.SIGINT:
-        logger.info("ARGOS master stopped!")
-
-
-def _set_environment_variables() -> None:
-    """
-    Load the environment variables from the nearest .env.argos file
-    """
-
-    # Find the .env file
-    env_path = find_dotenv(filename=".env.argos_master")
-
-    # Load the .env file
-    if env_path:
-        load_dotenv(dotenv_path=env_path)
-
-    # BASE_DIR validation
-    if not os.getenv("BASE_DIR"):
-        os.environ["BASE_DIR"] = AppDirs(__package__).user_data_dir
-
-    # Create BASE_DIR if it does not exist
-    os.makedirs(os.environ["BASE_DIR"], exist_ok=True)
-
-    # HOST validation
-    if not os.getenv("HOST"):
-        os.environ["HOST"] = "0.0.0.0"
-
-    # PORT validation
-    if not os.getenv("PORT"):
-        os.environ["PORT"] = "9876"
-
-
-def _get_logger() -> logging.Logger:
-    """
-    Get the logger
-    """
-
-    logger = logging.getLogger(__package__)
-    logger.setLevel(logging.INFO)
-
-    log_file_path = os.path.join(os.environ["BASE_DIR"], f"{__package__}.log")
-    os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
-    file_handler = logging.FileHandler(log_file_path)
-
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-    file_handler.setFormatter(formatter)
-
-    logger.addHandler(file_handler)
-
-    return logger
-
-
-def _launch_master(logger: logging.Logger) -> None:
-    """
-    Launch the GUI
-    """
-
-    if os.getenv("WERKZEUG_RUN_MAIN") == "true":
-        logger.info("ARGOS master started!")
 
     from . import gui  # pylint: disable=import-outside-toplevel
     from . import api  # pylint: disable=import-outside-toplevel
@@ -101,7 +26,6 @@ def _launch_master(logger: logging.Logger) -> None:
     socketio = SocketIO(app)
 
     # Add app configs
-    app.config["logger"] = logger
     app.config["WEBASSETS_CACHE"] = False
 
     # Register the API
@@ -110,39 +34,22 @@ def _launch_master(logger: logging.Logger) -> None:
     # Register the GUI
     gui.register(app)
 
+    # Register the signal handler
+    signal.signal(signal.SIGINT, lambda signum, frame: exit(0))
+
     socketio.run(
         app,
         host=os.environ["HOST"],
         port=int(os.environ["PORT"]),
         debug=False,
-        use_reloader=True,
+        use_reloader=os.getenv("HOT_RELOAD") == "true",
         log_output=False,
     )
 
 
-def main():
-    """
-    Main function of the program
-    """
-
-    # Set the environment variables
-    _set_environment_variables()
-
-    # Get the logger
-    logger = _get_logger()
-
-    # Register the exit callback
-    atexit.register(_exit_callback, logger)
-
-    # Register the global exception handler
-    sys.excepthook = _global_exception_hook(logger)
-
-    # Register the signal handler
-    signal.signal(signal.SIGINT, lambda signum, frame: exit(0))
-
-    # Launch the master
-    _launch_master(logger)
-
-
 if __name__ == "__main__":
+    if __package__ is None:
+        sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        __package__ = str("argos_master")  # pylint: disable=redefined-builtin
+
     main()
