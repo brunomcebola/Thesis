ec3b30d323f464a914df84fa1edfc356d5263921
Bruno Cebola <bruno.m.cebola@gmail.com>
2024-08-09 23:20:14 +0100

    BUG FIX (argos_node): Fixed error on camera launch due to hot reload

--- argos/argos_node/argos_node/routes/__init__.py
+++ argos/argos_node/argos_node/routes/__init__.py
@@ -2,10 +2,11 @@
 Init file for the routes module.
 """
 
+import os
+
 from .. import app as _app
 
-from . import cameras as _cameras
+if os.getenv("WERKZEUG_RUN_MAIN") == "true" or os.getenv("HOT_RELOAD") == "false":
+    from . import cameras as _cameras
 
-_app.register_blueprint(_cameras.blueprint)
-
-cleanup_cameras = _cameras.cleanup
+    _app.register_blueprint(_cameras.blueprint)
