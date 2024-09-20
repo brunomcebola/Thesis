781c5b1024cdf82287dedcd755d84f26192c6e26
Bruno Cebola <bruno.m.cebola@gmail.com>
2024-09-20 18:45:15 +0100

    FEATURE (kinetics): Addes way to create data for fine tuning

--- kinetics/generate_data.py
+++ kinetics/generate_data.py
@@ -21,7 +21,6 @@ from __future__ import print_function
 import gc
 import os
 import math
-import logging
 import numpy as np
 import tqdm
 
@@ -30,9 +29,8 @@ import tensorflow as tf  # pylint: disable=wrong-import-position
 
 import i3d  # pylint: disable=wrong-import-position
 
-_SAMPLES_PATH = "output"
-
-_LABEL_MAP_PATH = "label_map.txt"
+_SAMPLES_PATH = "npy/rgb"
+_OUTPUT_PATH = "data/rgb"
 
 _CHECKPOINTS_PATH = "checkpoints"
 
@@ -41,11 +39,6 @@ def main():
     """
     Main function to evaluate I3D on Kinetics.
     """
-
-    # hide tf logs
-    logging.basicConfig(level=logging.INFO)
-
-    kinetics_classes = [x.strip() for x in open(_LABEL_MAP_PATH, encoding="utf-8")]
 
     # Instantiate the model for RGB
     rgb_model = i3d.InceptionI3d(400, spatial_squeeze=True, final_endpoint="Mixed_5c")
@@ -63,129 +56,84 @@ def main():
         os.path.join(_CHECKPOINTS_PATH, "flow", "model.ckpt")
     ).expect_partial()
 
-    # Perform the inferences
-    inferences = []
-    for folder in tqdm.tqdm(
-        [
-            folder
-            for folder in os.listdir(_SAMPLES_PATH)
-            if os.path.isdir(os.path.join(_SAMPLES_PATH, folder))
-        ]
-    ):
-        # RGB prediction
-        if not os.path.exists(os.path.join(_SAMPLES_PATH, folder, f"{folder}_rgb.npy")):
-            raise FileNotFoundError(f"RGB sample not found: {folder}_rgb.npy")
+    for cls in os.listdir(_SAMPLES_PATH):
+        print(f"Generating data for: {cls}")
 
-        rgb_sample = tf.convert_to_tensor(
-            np.load(os.path.join(_SAMPLES_PATH, folder, f"{folder}_rgb.npy")), dtype=tf.float32
-        )
+        samples = os.listdir(os.path.join(_SAMPLES_PATH, cls))
 
-        transposed = False
-        if rgb_sample.shape[2] > rgb_sample.shape[3]:  # type: ignore
-            rgb_sample = tf.transpose(rgb_sample, (0, 1, 3, 2, 4))
-            transposed = True
+        for sample in tqdm.tqdm(samples):
 
-        rgb_samples = []
+            os.makedirs(os.path.join(_OUTPUT_PATH, cls, sample), exist_ok=True)
 
-        final = rgb_sample.shape[3] - 224 + 1  # type: ignore
-        step = int((rgb_sample.shape[3] - 224) / math.floor(rgb_sample.shape[3] / 224)) - 1  # type: ignore # pylint: disable=line-too-long
-        for i in range(0, final, step if step > 0 else 1):
-            rgb_samples.append(rgb_sample[:, :, :, i : i + 224, :])  # type: ignore
+            rgb_samples = []
 
-        if transposed:
-            for i, sample in enumerate(rgb_samples):
-                rgb_samples[i] = tf.transpose(sample, (0, 1, 3, 2, 4))
-
-
-
-
-
-
-
-
-        rgb_out, _ = rgb_model(rgb_samples[0])
-        print(rgb_out.shape)
-
-        for sample in rgb_samples[1:]:
-            out, _ = rgb_model(sample)
-            print(out.shape)
-
-
-
-
-        exit()
-
-        rgb_predictions = tf.nn.softmax(rgb_logits)[0]
-
-        tf.keras.backend.clear_session()
-
-        rgb_index = np.argsort(rgb_predictions)[::-1][0]
-        rgb_prediction = rgb_predictions[rgb_index]
-        rgb_label = kinetics_classes[rgb_index]
-
-        # Flow prediction
-        if not os.path.exists(os.path.join(_SAMPLES_PATH, folder, f"{folder}_flow.npy")):
-            raise FileNotFoundError(f"Flow sample not found: {folder}_flow.npy")
-
-        flow_sample = tf.convert_to_tensor(
-            np.load(os.path.join(_SAMPLES_PATH, folder, f"{folder}_flow.npy")), dtype=tf.float32
-        )
-
-        transposed = False
-        if flow_sample.shape[2] > flow_sample.shape[3]:  # type: ignore
-            flow_sample = tf.transpose(flow_sample, (0, 1, 3, 2, 4))
-            transposed = True
-
-        flow_samples = []
-
-        final = flow_sample.shape[3] - 224 + 1  # type: ignore
-        step = int((flow_sample.shape[3] - 224) / math.floor(flow_sample.shape[3] / 224)) - 1  # type: ignore # pylint: disable=line-too-long
-        for i in range(0, final, step if step > 0 else 1):
-            flow_samples.append(flow_sample[:, :, :, i : i + 224, :])  # type: ignore
-
-        if transposed:
-            for i, sample in enumerate(flow_samples):
-                flow_samples[i] = tf.transpose(sample, (0, 1, 3, 2, 4))
-
-        flow_logits, _ = flow_model(flow_samples[0])
-        for sample in flow_samples[1:]:
-            logits, _ = flow_model(sample)
-            flow_logits += logits
-
-        flow_predictions = tf.nn.softmax(flow_logits)[0]
-
-        flow_index = np.argsort(flow_predictions)[::-1][0]
-        flow_prediction = flow_predictions[flow_index]
-        flow_label = kinetics_classes[flow_index]
-
-        # Joint prediction
-        joint_logits = rgb_logits + flow_logits
-        joint_predictions = tf.nn.softmax(joint_logits)[0]
-
-        joint_index = np.argsort(joint_predictions)[::-1][0]
-        joint_prediction = joint_predictions[joint_index]
-        joint_label = kinetics_classes[joint_index]
-
-        inferences.append(
-            (
-                folder,
-                (rgb_label, rgb_prediction),
-                (flow_label, flow_prediction),
-                (joint_label, joint_prediction),
+            # RGB
+            rgb_sample = tf.convert_to_tensor(
+                np.load(os.path.join(_SAMPLES_PATH, cls, sample, f"{sample}_rgb.npy")),
+                dtype=tf.float32,
             )
-        )
 
-        # Clean up GPU memory after each folder inference
-        del rgb_samples, rgb_logits, rgb_sample
-        del flow_samples, flow_logits, flow_sample
-        gc.collect()
-        tf.keras.backend.clear_session()
+            transposed = False
+            if rgb_sample.shape[2] > rgb_sample.shape[3]:  # type: ignore
+                rgb_sample = tf.transpose(rgb_sample, (0, 1, 3, 2, 4))
+                transposed = True
 
-    for inference in inferences:
-        print(f"\nPrediction for {inference[0]}:")
-        print(f"\tRGB: '{inference[1][0]}' ({inference[1][1]})")
-        print(f"\tFlow: '{inference[2][0]}' ({inference[2][1]})")
-        print(f"\tJoint: '{inference[3][0]}' ({inference[3][1]})")
+            rgb_sub_samples = []
+
+            final = rgb_sample.shape[3] - 224 + 1  # type: ignore
+            step = int((rgb_sample.shape[3] - 224) / math.floor(rgb_sample.shape[3] / 224)) - 1  # type: ignore # pylint: disable=line-too-long
+            for i in range(0, final, step if step > 0 else 1):
+                rgb_sub_samples.append(rgb_sample[:, :, :, i : i + 224, :])  # type: ignore
+
+            if transposed:
+                for i, sample in enumerate(rgb_sub_samples):
+                    rgb_sub_samples[i] = tf.transpose(sample, (0, 1, 3, 2, 4))
+
+            for ssi, sub_sample in enumerate(rgb_sub_samples):
+                out, _ = rgb_model(sub_sample)
+
+                np.save(
+                    os.path.join(_OUTPUT_PATH, cls, sample, f"{sample}_{ssi}_rgb.npy"), out.numpy()
+                )
+
+                del out
+
+            # Flow
+            flow_sample = tf.convert_to_tensor(
+                np.load(os.path.join(_SAMPLES_PATH, cls, sample, f"{sample}_flow.npy")),
+                dtype=tf.float32,
+            )
+
+            transposed = False
+            if flow_sample.shape[2] > flow_sample.shape[3]:  # type: ignore
+                flow_sample = tf.transpose(flow_sample, (0, 1, 3, 2, 4))
+                transposed = True
+
+            flow_sub_samples = []
+
+            final = flow_sample.shape[3] - 224 + 1  # type: ignore
+            step = int((flow_sample.shape[3] - 224) / math.floor(flow_sample.shape[3] / 224)) - 1  # type: ignore # pylint: disable=line-too-long
+            for i in range(0, final, step if step > 0 else 1):
+                flow_sub_samples.append(flow_sample[:, :, :, i : i + 224, :])  # type: ignore
+
+            if transposed:
+                for i, sample in enumerate(flow_sub_samples):
+                    flow_sub_samples[i] = tf.transpose(sample, (0, 1, 3, 2, 4))
+
+            for ssi, sub_sample in enumerate(flow_sub_samples):
+                out, _ = flow_model(sub_sample)
+
+                np.save(
+                    os.path.join(_OUTPUT_PATH, cls, sample, f"{sample}_{ssi}_flow.npy"), out.numpy()
+                )
+
+                del out
+
+            # Clean up GPU memory after each folder inference
+            del rgb_sample, rgb_sub_samples
+            del flow_sample, flow_sub_samples
+            gc.collect()
+            tf.keras.backend.clear_session()
 
 
 if __name__ == "__main__":
